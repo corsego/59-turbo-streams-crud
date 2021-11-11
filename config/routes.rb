70d81e9399f8e204b8a6498d9c69135b88caddad
Yaro <yshmarov@gmail.com>
2021-11-11 18:15:50 +0000

    scaffold messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  root "messages#index"
+  resources :messages
 end
